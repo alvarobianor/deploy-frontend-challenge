397ac9ba90076fbc8a8ff3c332ab0cffd9e0c924
alvaro bianor <bianor.alvaro12@gmail.com>
2021-06-06 20:58:00 -0300

    Set new url to db

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-const protocol = "http://";
-const port = 3333;
+// const protocol = "http://";
+// const port = 3333;
 
-export const host = `${protocol}localhost:${port}/`;
+export const host = `https://floating-lake-78816.herokuapp.com/`;
